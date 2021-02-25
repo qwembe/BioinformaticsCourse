21073d9f7d30a1abbc5dc56ca8706f22233b72f8
Андрей Вайгачёв <qwembe@gmail.com>
2021-02-25 16:04:27 +0300

    Revert "Bioinformatics vaigachev 1 2"

--- 1_2/main.py
+++
@@ -1,8 +0,0 @@
-from Vunsh import *
-import Bio.Align.substitution_matrices as substitution_matrices
-
-MATRIX = substitution_matrices.load("BLOSUM62")
-
-fr = Wunsch("AGC", "A",MATRIX)
-print(fr)
-
